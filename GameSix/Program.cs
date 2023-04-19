70ee6d9143283529b71332df1d7f6071c4b1adcf
Rafael José <ra.david@hotmail.com>
2023-04-19 12:51:50 +0100

    Store the foes in an array

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -9,6 +9,9 @@ namespace GameSix
             // Ask user for the number of foes
             Console.Write("How many foes? ");
             int numberOfFoes = int.Parse(Console.ReadLine());
+
+            // Store the foes in an array
+            Foe[] foes = new Foe[numberOfFoes];
         }
     }
 }
