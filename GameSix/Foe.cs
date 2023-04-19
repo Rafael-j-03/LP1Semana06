feb8da2f2a14306ae4ab6a67a92702c453755961
Rafael José <ra.david@hotmail.com>
2023-04-19 12:30:42 +0100

    Add constructor to 'Foe'

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -7,6 +7,16 @@ namespace GameSix
 {
     public class Foe
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        // Constructor
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
